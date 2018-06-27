9cf2b3b1ba813f5345debc87f39ecfdd931699aa
nbfigueroa <nbfigueroa@gmail.com>
2018-06-28 01:16:56 +0200

    Removed some anmnoying comments

--- utils/model-selection/ml_curve_opt.m
+++ utils/model-selection/ml_curve_opt.m
@@ -38,41 +38,41 @@ switch(method)
    % %%%% Using distance to line
     case 'line'
 
-        %# get coordinates of all the points
+        % get coordinates of all the points
         nPoints = length(curve);
         allCoord = [1:nPoints;curve]';              %'# SO formatting
 
-        %# pull out first point
+        % pull out first point
         firstPoint = allCoord(1,:);
 
-        %# get vector between first and last point - this is the line
+        % get vector between first and last point - this is the line
         lineVec = allCoord(end,:) - firstPoint;
 
-        %# normalize the line vector
+        % normalize the line vector
         lineVecN = lineVec / sqrt(sum(lineVec.^2));
 
-        %# find the distance from each point to the line:
-        %# vector between all points and first point
+        % find the distance from each point to the line:
+        % vector between all points and first point
         vecFromFirst = bsxfun(@minus, allCoord, firstPoint);
 
-        %# To calculate the distance to the line, we split vecFromFirst into two 
-        %# components, one that is parallel to the line and one that is perpendicular 
-        %# Then, we take the norm of the part that is perpendicular to the line and 
-        %# get the distance.
-        %# We find the vector parallel to the line by projecting vecFromFirst onto 
-        %# the line. The perpendicular vector is vecFromFirst - vecFromFirstParallel
-        %# We project vecFromFirst by taking the scalar product of the vector with 
-        %# the unit vector that points in the direction of the line (this gives us 
-        %# the length of the projection of vecFromFirst onto the line). If we 
-        %# multiply the scalar product by the unit vector, we have vecFromFirstParallel
+        % To calculate the distance to the line, we split vecFromFirst into two 
+        % components, one that is parallel to the line and one that is perpendicular 
+        % Then, we take the norm of the part that is perpendicular to the line and 
+        % get the distance.
+        % We find the vector parallel to the line by projecting vecFromFirst onto 
+        % the line. The perpendicular vector is vecFromFirst - vecFromFirstParallel
+        % We project vecFromFirst by taking the scalar product of the vector with 
+        % the unit vector that points in the direction of the line (this gives us 
+        % the length of the projection of vecFromFirst onto the line). If we 
+        % multiply the scalar product by the unit vector, we have vecFromFirstParallel
         scalarProduct = dot(vecFromFirst, repmat(lineVecN,nPoints,1), 2);
         vecFromFirstParallel = scalarProduct * lineVecN;
         vecToLine = vecFromFirst - vecFromFirstParallel;
 
-        %# distance to line is the norm of vecToLine
+        % distance to line is the norm of vecToLine
         distToLine = sqrt(sum(vecToLine.^2,2));
 
-        %# find the maximum
+        % find the maximum
         [~,opt_ids] = max(distToLine);
         opt_vals = curve(opt_ids);
 
